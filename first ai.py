657f257aafc5804cde7ca4be6944abbd6bc17e03
Sahil Amin <48405411+Sahilamin219@users.noreply.github.com>
2020-01-16 14:31:08 +0530

    Add files via upload

---
+++ first ai.py
@@ -0,0 +1,11 @@
+# import gym
+# import universe
+# # define an enviroment variable
+# env = gym.make('flashgames.CoasterRacer-v0')
+# observation_n = env.reset()
+
+# while True:
+# 	action_n = [[('keyEvent', 'Arrowup', True)] for observation_n]
+# 	observation_n, reward_n, done_n, info = env.step(action_n)
+# 	env.render()
+
